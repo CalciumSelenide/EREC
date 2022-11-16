ef6b374dd532935e871c1c618f22c7be06693f3c
CalciumSelenide <117214153+CalciumSelenide@users.noreply.github.com>
2022-11-16 15:10:50 -0600

    serialInterface.py is created
    This script sits on a laptop connected to a Micro:Bit that is configured to be a radio transceiver. It makes an attempt to connect to the Micro:Bit, then (if successful) starts live logging the arrowkeys that are pressed, sending them to the device.

---
+++ CuteBot/RemoteControl/serialInterface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# CalciumSelenide - Wed Nov 12 02:32:55 PM CST 2022
+import serial
+import serial.tools.list_ports as list_ports
+from microBitController import arrowController
+
+if __name__ == "__main__":
+    # Try to establish communication with the microbit connected to your laptop serially
+    try:
+        microBit = serial.Serial(port='/dev/ttyACM0', baudrate='115200', timeout=0.1)
+    except Exception as e:
+        print("WARNING: " + str(e))
+    
+    # If there is a microbit that is reachable ...
+    if microBit:
+        # ... implement the arrowController function
+        arrowController(microBit)
+    else:
+        print("No Micro:Bit was found! :(")
