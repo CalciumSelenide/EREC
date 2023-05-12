f6afce1e2946f0d4a5bef28e4936bbd683d46cf2
CalciumSelenide <117214153+CalciumSelenide@users.noreply.github.com>
2023-05-11 20:39:59 -0500

    Create sprites.py

---
+++ Pygame/Lesson1/sprites.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+################# SUPRESS PYGAME SUPPORT MESSAGE ###################
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+####################################################################
+
+import pygame, time
+
+class Sprite():
+    def __init__(self, x, y, w, h, img):
+        self.identity = None
+
+        # Load the position and size of the sprite
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+        # Simple booleans to tell us the direction the sprite is facing
+        self.horizontalFlip = False
+        self.verticalFlip = False
+
+        # Load the sprite image
+        self.spriteImage = pygame.image.load(img)
+
+    def update(self):
+        pass
+
+    def identity(self):
+        return None
+
+class Clyde(Sprite):
+    def __init__(self, x, y):
+        # Initialize Clyde and set his identity 
+        super().__init__(x, y, 66, 92, "imgs/p1_front.png")
+        self.identity = "CLYDE"
+
+    def addSprite(self, view, sprite):
+        # Clyde needs to show up in our world!
+        view.screen.blit(pygame.transform.flip(sprite.spriteImage, sprite.horizontalFlip, sprite.verticalFlip),
+                         (sprite.x, sprite.y))
+
+    def identify(self):
+        return self.identity
